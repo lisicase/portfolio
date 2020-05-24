417641a96ac8297d266d94988216ca2a55589b1f
lisicase <casee@uw.edu>
2020-05-23 23:40:42 -0700

    filled in about page; attempted to add a carousel

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,29 @@
 
 let carousel = document.querySelector("carousel");
 if (carousel != null) {
-    
+    let nextBtn = document.querySelector("a.carousel-control-next");
+    let prevBtn = document.querySelector("a.carousel-control-prev");
+
+    nextBtn.addEventListener("click", cycleForward);
+    prevBtn.addEventListener("click", cycleBackward);
+
+    let indicators = document.querySelector("ol.carousel-indicators");
+    for (let i = 0; i < indicators.childElementCount; i++) {
+        let indicator = document.querySelector("ol.carousel-indicators:nthchild:(" + i + ")");
+        indicator.addEventListener("click", function() {
+            goToSlide(i);
+        });
+    }
+}
+
+function cycleForward() {
+    carousel.carousel('next');
+}
+
+function cycleBackward() {
+    carousel.carousel('prev');
+}
+
+function goToSlide(slideNum) {
+    carousel.carousel(number);
 }
