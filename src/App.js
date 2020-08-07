a89f29203998e887a7c9c29b21a4b5bc23db4338
lisicase <casee@uw.edu>
2020-08-06 17:25:00 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
     </div>
   );
 }
@@ -25,27 +17,15 @@ class NavBar extends Component {
   render() {
     return (
       <header>
-        <nav className="navbar navbar-expand-lg navbar-light">
-            <a className="nav-link" href="portfolio.html"><h1 className="nav-name">ELISABETH (LISI) CASE</h1></a>
-            <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span><i className="fa fa-bars"></i></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link" href="portfolio.html">Portfolio<span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="experience.html">Experience</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="about.html">About</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
+        <Navbar>
+          <Navbar.Brand href="#portfolio"><h1 className="nav-name">ELISABETH (LISI) CASE</h1></Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+          </Nav>
+        </Navbar>
+      </header>
     );
   }
 }
