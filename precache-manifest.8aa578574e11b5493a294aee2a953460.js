5a9eb45a2142e1a65d24ac8ede877413d1139945
lisicase <casee@uw.edu>
2020-08-18 21:31:41 -0700

    Updates

---
+++ precache-manifest.8aa578574e11b5493a294aee2a953460.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d869e43bb50f2b990c929d8bdbc4bf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "330af621f93dbfda56dd",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9dd9885ccc883c6a21e4",
+    "url": "/portfolio/static/css/main.66a7d3bd.chunk.css"
+  },
+  {
+    "revision": "330af621f93dbfda56dd",
+    "url": "/portfolio/static/js/2.8e3cdd25.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/portfolio/static/js/2.8e3cdd25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dd9885ccc883c6a21e4",
+    "url": "/portfolio/static/js/main.d44922c0.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "f1314063812ecd106df851d250b3a8f4",
+    "url": "/portfolio/static/media/ElisabethCase-PM.f1314063.pdf"
+  },
+  {
+    "revision": "d3923445969a33b5a6627e636f3084c2",
+    "url": "/portfolio/static/media/ElisabethCase-SWE.d3923445.pdf"
+  }
+]);
